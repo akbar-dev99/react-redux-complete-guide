2fbe3935940274122307c668ba8ace11d899bc9b
Unit-Git <theunit954@gmail.com>
2021-02-10 16:36:39 +0700

    koneksi react ke redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux";
+import { reducer } from "./store/reducer";
+
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+
+const store = createStore();
+
+const initStore = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(initStore, document.getElementById("root"));
 registerServiceWorker();
