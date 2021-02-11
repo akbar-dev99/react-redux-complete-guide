2535861114a248194c08147a8e9f5eaa88975805
Unit-Git <theunit954@gmail.com>
2021-02-11 13:50:35 +0700

    menghapus beberapa line yang tidak diperlukan

--- src/index.js
+++ src/index.js
@@ -9,14 +9,7 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-const composeEnhancers =
-  (typeof window !== "undefined" &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
-
 const store = createStore(reducer);
-
-console.log(store);
 
 const initStore = (
   <Provider store={store}>
